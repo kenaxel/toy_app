fd178fc0bcd00241cfb4078dde45d2c09dd60a51
Ken <ec2-user@ip-172-31-47-130.ap-southeast-1.compute.internal>
2019-06-14 06:24:15 +0000

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!あああ"
+    render html: "hello, world! アホアホ"
   end
 end
